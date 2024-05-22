d0e6e08c256c210c4da96a0ae754e4d6461da406
Sachin Tripathi <190113027@hbtu.ac.in>
2024-05-22 15:41:58 +0530

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,11 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-	projectId: "7cmk1q",
+	projectId: "h7b2g9",
 	defaultCommandTimeout: 40000,
 	viewportWidth: 1920,
 	viewportHeight: 1080,
+	headless: true
 
 	e2e: {
 		baseUrl: 'https://d.assess.wecreateproblems.com/'
